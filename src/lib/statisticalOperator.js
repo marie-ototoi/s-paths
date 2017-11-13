5738e0ff5671889bdbccc3d621f91a0eeab04dfb
dlebout <dylan.lebout@gmail.com>
2017-11-13 15:37:28 +0100

    add statistOperator

---
+++ src/lib/statisticalOperator.js
@@ -0,0 +1,60 @@
+const findDateRatio = (data) => {
+
+}
+const findTextRatio = (data) => {
+
+}
+const findBestRatioSingleProp = (data) => {
+
+}
+
+const findBestRatioDoubleProps = (data, processing = 'RAW') => {
+    switch (processing) {
+    case 'RAW':
+
+        break
+    default:
+    }
+}
+
+const computeStatisticalInformation = (data) => {
+    let statistics = []
+    let total = 0
+    let res = []
+    // Numbeer of props
+    data.statements.results.bindings.forEach(function (item) {
+        let prop1 = item.prop1.value
+        let prop2 = item.prop2.value
+        let index1 = (Number(prop1) - Number(prop1) % 1) + ''
+        if (index1 in statistics) {
+            if (prop2 in statistics[index1]) {
+                statistics[index1][prop2] = statistics[index1][prop2] + 1
+                total++
+            } else {
+                statistics[index1][prop2] = 1
+                total++
+            }
+        } else {
+            statistics[index1] = []
+            statistics[index1][prop2] = 1
+            total++
+        }
+    })
+    let minValue = 1
+    let maxValue = 0
+    for (let k1 in statistics) {
+        for (let k2 in statistics[k1]) {
+            statistics[k1][k2] = statistics[k1][k2] / total
+            if (statistics[k1][k2] > maxValue) maxValue = statistics[k1][k2]
+            if (statistics[k1][k2] < minValue) minValue = statistics[k1][k2]
+            let newItem = {}
+            newItem.prop1 = k1
+            newItem.prop2 = k2
+            newItem.value = statistics[k1][k2]
+            res.push(newItem)
+        }
+    }
+    return { data: res, min: minValue, max: maxValue }
+}
+
+exports.computeStatisticalInformation = computeStatisticalInformation
