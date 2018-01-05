086898ab5f7dabcce456ebbfb6fbc36ecdea5390
Marie Destandau <marie.destandau@inria.fr>
2018-01-05 16:28:00 +0100

    Nav layout

--- src/components/elements/Nav.jsx
+++ src/components/elements/Nav.jsx
@@ -15,7 +15,7 @@ class Nav extends React.PureComponent {
         const itemWidth = this.props.dimensions.width / 6
         const itemHeight = itemWidth * 3 / 4
         const margin = itemWidth / 6
-        const maxBarWidth = (itemWidth * 4) + (margin * 3)
+        const maxBarWidth = (itemWidth * 3) + (margin * 2)
 
         // console.log(dataset.labels)
         return (<g className = "Nav"
@@ -28,9 +28,9 @@ class Nav extends React.PureComponent {
             { options.map((option, i) => {
                 const barWidth = maxBarWidth * option.total / options[0].total
                 return <g key = { this.props.zone + '_summary_' + i }>
-                    <rect width = { barWidth } height = { 12 } y = { 60 + margin + i * 16 } x = { margin + maxBarWidth - barWidth } fill = "#E0E0E0"></rect>
-                    <text textAnchor = "end" x = { maxBarWidth + margin - 2 } fill = "#666666" y = { 60 + margin + 10 + i * 16 }>{ option.label }</text>
-                    <text x = { maxBarWidth + margin * 2 } fill = "#333333" y = { 60 + margin + 10 + i * 16 }>{ option.total }</text>
+                    <rect width = { barWidth } height = { 12 } y = { 60 + margin + i * 16 } x = { margin * 2 + itemWidth } fill = "#E0E0E0"></rect>
+                    <text x = { margin } fill = "#666666" y = { 60 + margin + 10 + i * 16 }>{ option.label }</text>
+                    <text x = { maxBarWidth + margin * 3 + itemWidth } fill = "#333333" y = { 60 + margin + 10 + i * 16 }>{ option.total }</text>
                 </g>
             }) }
         </g>)
