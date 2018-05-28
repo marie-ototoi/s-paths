dfddb21aa93c0d66022f1828a8fef2ac4ffc977c
Léo Colombaro <leo.colombaro@lri.fr>
2018-05-28 16:59:44 +0200

    Add a cleaner webpack plugin to ensure empty dist folder before dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,11 @@
 const merge = require('webpack-merge')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'cheap-module-source-map'
+    devtool: 'cheap-module-source-map',
+    plugins: [
+        new CleanWebpackPlugin(common.output.path)
+    ]
 })
