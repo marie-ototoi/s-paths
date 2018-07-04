11685771a416a353d4002486ce389624991ed050
Marie Destandau <marie.destandau@inria.fr>
2018-07-04 16:56:32 +0200

    Mongo connection pb

--- models/connection.js
+++ models/connection.js
@@ -3,5 +3,8 @@ import mongoose from 'mongoose'
 mongoose.Promise = Promise
 
 mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI, {
+        autoReconnect: true,
+        reconnectTries: 20,
+    })
     .catch(err => console.error('✘ CANNOT CONNECT TO MongoDB DATABASE !', err))
