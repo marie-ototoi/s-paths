f8fab3fa6b6a0c3d45dddf93af1bf50c1bf03fd8
Marie Destandau <marie.destandau@inria.fr>
2018-09-13 15:49:10 +0200

    Clean propSelector

--- src/components/elements/PropSelector.jsx
+++ src/components/elements/PropSelector.jsx
@@ -1,29 +1,13 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import ReactSelect from 'react-select'
 import { connect } from 'react-redux'
-
-import { selectResource, selectProperty } from '../../actions/dataActions'
 
 class PropSelector extends React.PureComponent {
     constructor (props) {
         super(props)
-        this.handleSelect = this.handleSelect.bind(this)
         this.state = {
             elementName: `${props.zone}_selector_${props.propIndex}`,
             selected: props.selected
-        }
-    }
-    handleSelect (e) {
-        const { config, dataset, propIndex, propList, views, zone } = this.props
-        this.setState({
-            selected: this.props.selected
-        })
-        let requestedProp = propList[e.value]
-        if (this.props.type === 'header') {
-            this.props.selectResource({ ...dataset, entrypoint: requestedProp.path, totalInstances: requestedProp.total, constraints: `` }, views)
-        } else {
-            this.props.selectProperty(propIndex, requestedProp.path, config, dataset, zone)
         }
     }
     render () {
@@ -42,33 +26,15 @@ class PropSelector extends React.PureComponent {
                 width = { width }
                 height = { height }
             >
-                { (!this.state.selected) &&
-                    <p
-                        className = { alignClass }
-                        onClick = { (e) => this.setState({ selected: true }) }
-                    >/ 
-                        { selectedProp && selectedProp.readablePath.map((part, index) => {
-                            return <span key = { `${this.state.elementName}_path_${index}` }>
-                                &nbsp;<span title = { part.comment }>{part.label} </span>  { (index < selectedProp.readablePath.length - 1) ? ' / * / ' : ' /' }
-                            </span>
-                        }) }
-                    </p>
-                }
-                { (this.state.selected) &&
-                <ReactSelect
-                    classNamePrefix = "propSelector"
-                    placeholder = {'/ ' + propList[selectedPropIndex].readablePath.map(part => part.label).join(' / * / ').concat(' /')}
-
-                    value = { propList[selectedPropIndex].readablePath.map(part => part.label).join(' / ') }
-                    onChange = { this.handleSelect }
-                    options = {propList.map((config, i) => {
-                        return {
-                            label: '/ ' + config.readablePath.map(part => part.label).join(' / * / ').concat(config.total ? ' (' + config.total + ')' : ' /') ,
-                            value: i
-                        }
-                    })}
-                />
-                }
+                <p
+                    className = { alignClass }
+                >/ 
+                    { selectedProp && selectedProp.readablePath.map((part, index) => {
+                        return <span key = { `${this.state.elementName}_path_${index}` }>
+                            &nbsp;<span title = { part.comment }>{part.label} </span>  { (index < selectedProp.readablePath.length - 1) ? ' / * / ' : ' /' }
+                        </span>
+                    }) }
+                </p>
             </foreignObject>
         )
     }
@@ -100,10 +66,7 @@ function mapStateToProps (state) {
 }
 
 function mapDispatchToProps (dispatch) {
-    return {
-        selectProperty: selectProperty(dispatch),
-        selectResource: selectResource(dispatch)
-    }
+    return {}
 }
 
 const PropSelectorConnect = connect(mapStateToProps, mapDispatchToProps)(PropSelector)
