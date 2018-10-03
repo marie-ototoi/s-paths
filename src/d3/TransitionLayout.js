73ec5adf8923a386c4c8709f5b95d76c6740efe4
Marie Destandau <marie.destandau@inria.fr>
2018-10-03 16:30:37 +0200

    Fix transition

--- src/d3/TransitionLayout.js
+++ src/d3/TransitionLayout.js
@@ -3,22 +3,16 @@ import AbstractLayout from './AbstractLayout'
 
 class TransitionLayout extends AbstractLayout {
     draw (props) {
-        this.drawShapes(props, props.elements.origin, 'origin')
-        this.drawShapes(props, props.elements.target, 'target')
-        
-        // console.log(props.elements.origin.length, props.elements.target.length)
-        // console.log('meoui',  props.elements.target)
-    }
-    drawShapes (props, shapes, type) {
+
         // console.log(`|||||drawRectangles`, props.zone, type)
-        const shapesSelection = d3.select(this.el).selectAll('rect')
-            .data(shapes, (d) => d.signature)
+        let shapesSelection = d3.select(this.el).selectAll('rect')
+            .data(props.elements.origin, (d) => d.signature)
         const tChange = d3.transition()
             .duration(750)
         const tRemove = d3.transition()
-            .duration(500)
+            .duration(750)
         const tEnter = d3.transition()
-            .duration(250)
+            .duration(750)
 
         shapesSelection
             .enter()
@@ -29,27 +23,45 @@ class TransitionLayout extends AbstractLayout {
             .attr('height', d => d.zone.height)
             .attr('fill', d => d.color)
             .attr('transform', d => `rotate(${d.rotation} ${d.zone.x1} ${d.zone.y1})`)
-            .attr('fill-opacity', d => type === 'target' ? 0 : d.opacity)
-            .transition(tEnter)
-            .attr('fill-opacity', d => d.opacity)
-          
-        shapesSelection
-            .exit()
-            .transition(tRemove)
-            .attr('fill-opacity', 0)
-            .remove()           
+            .attr('opacity', d => d.opacity)
 
-        d3.select(this.el).selectAll('rect')
-            .transition(tChange)
-            .attr('x', d => d.zone.x1)
-            .attr('y', d => d.zone.y1)
-            .attr('width', d => d.zone.width)
-            .attr('height', d => d.zone.height)
-            .attr('transform', d => `rotate(${d.rotation} ${d.zone.x1} ${d.zone.y1})`)
-            .attr('fill', d => d.color)
-            .attr('fill-opacity', 1)
+        setTimeout(() => {
+            shapesSelection = d3.select(this.el).selectAll('rect')
+                .data(props.elements.target, (d) => d.signature)
             
-        if (type === 'target') window.setTimeout(() => { props.endTransition(props.zone) }, 1000)
+            shapesSelection
+                .transition(tChange)
+                .attr('x', d => d.zone.x1)
+                .attr('y', d => d.zone.y1)
+                .attr('width', d => d.zone.width)
+                .attr('height', d => d.zone.height)
+                .attr('transform', d => `rotate(${d.rotation} ${d.zone.x1} ${d.zone.y1})`)
+                .attr('fill', d => d.color)
+    
+            shapesSelection
+                .enter()
+                .append('rect')
+                .attr('x', d => d.zone.x1)
+                .attr('y', d => d.zone.y1)
+                .attr('width', d => d.zone.width)
+                .attr('height', d => d.zone.height)
+                .attr('fill', d => d.color)
+                .attr('transform', d => `rotate(${d.rotation} ${d.zone.x1} ${d.zone.y1})`)
+                .attr('opacity', d => 0)
+                .transition(tEnter)
+                .attr('opacity', d => d.opacity)
+              
+            shapesSelection
+                .exit()
+                .transition(tRemove)
+                .attr('opacity', 0)
+                .remove()   
+        }, 10)
+
+               
+
+            
+        window.setTimeout(() => { props.endTransition(props.zone) }, 1000)
         // console.log('drawn')
     }
     checkSelection (props) {}
