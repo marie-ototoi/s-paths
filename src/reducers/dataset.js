ee7973002f03b802b08b4c8d03eb7cbfba188bb3
Marie Destandau <marie.destandau@inria.fr>
2019-09-24 13:46:34 +0200

    besdui named graph

--- src/reducers/dataset.js
+++ src/reducers/dataset.js
@@ -159,7 +159,7 @@ if (process.env.APP_NAME === 'bnf') {
 }  else if (process.env.APP_NAME === 'besdui') { 
     initialState = {
         ...initialState,
-        graphs: ['http://besdui.ilda.fr'],
+        graphs: ['urn:bsbm'],
         prefixes: {
             rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
             rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
